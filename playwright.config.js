c033f2c88e21b8ad750fe0f2463f9ddb49a8e6f5
Thang Le <thangle@Thangs-MacBook-Air.local>
2025-08-08 11:12:05 +0700

    Initial commit

---
+++ playwright.config.js
@@ -0,0 +1,23 @@
+// playwright.config.js
+module.exports = {
+  timeout: 30000,
+  use: {
+    headless: true,
+    viewport: { width: 1280, height: 720 },
+    ignoreHTTPSErrors: true,
+  },
+  projects: [
+    {
+      name: 'Chromium',
+      use: { browserName: 'chromium' },
+    },
+    {
+      name: 'Firefox',
+      use: { browserName: 'firefox' },
+    },
+    {
+      name: 'WebKit',
+      use: { browserName: 'webkit' },
+    },
+  ],
+};
